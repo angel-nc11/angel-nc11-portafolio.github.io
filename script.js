ad13426a523a715c1593c5fece7e7b9f4bbda085
angel-nc11 <anajarro572@gmail.com>
2023-03-30 20:27:41 -0600

    responsive para celular

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
 document.querySelector(".encriptador").style.display = "none";
 document.querySelector(".barberia").style.display = "none";
 
-function ocultarBarberia() {
-    var x = document.querySelector(".divisas");
-	var y = document.querySelector(".barberia");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-		y.style.display = "none"
-    } else {
-        x.style.display = "none";
-		y.style.display = "block";
-
-    }
-}
-
-document.querySelector('.btm-divisas').onclick = ocultarBarberia;
-document.querySelector('.btm-barberia').onclick = ocultarBarberia;
 
 function ocultarTorres() {
     var x = document.querySelector(".torres");
@@ -34,6 +19,21 @@ document.querySelector('.btm-hanoi').onclick = ocultarTorres;
 document.querySelector('.btm-encriptador').onclick = ocultarTorres;
 
 
+function ocultarBarberia() {
+    var x = document.querySelector(".divisas");
+	var y = document.querySelector(".barberia");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+		y.style.display = "none"
+    } else {
+        x.style.display = "none";
+		y.style.display = "block";
+
+    }
+}
+
+document.querySelector('.btm-divisas').onclick = ocultarBarberia;
+document.querySelector('.btm-barberia').onclick = ocultarBarberia;
 
 
 
