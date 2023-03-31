f6cb779351f670c1acc792601b20f2cac74c2736
angel-nc11 <anajarro572@gmail.com>
2023-03-30 20:11:41 -0600

    responsive

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
 document.querySelector(".encriptador").style.display = "none";
 document.querySelector(".barberia").style.display = "none";
 
-function ocultarTorres() {
-    var x = document.querySelector(".torres");
-	var y = document.querySelector(".encriptador");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-		y.style.display = "none"
-    } else {
-        x.style.display = "none";
-		y.style.display = "block";
-
-    }
-}
-document.querySelector('.btm-hanoi').onclick = ocultarTorres;
-document.querySelector('.btm-encriptador').onclick = ocultarTorres;
-
-
 function ocultarBarberia() {
     var x = document.querySelector(".divisas");
 	var y = document.querySelector(".barberia");
@@ -32,6 +16,23 @@ function ocultarBarberia() {
 
 document.querySelector('.btm-divisas').onclick = ocultarBarberia;
 document.querySelector('.btm-barberia').onclick = ocultarBarberia;
+
+function ocultarTorres() {
+    var x = document.querySelector(".torres");
+	var y = document.querySelector(".encriptador");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+		y.style.display = "none"
+    } else {
+        x.style.display = "none";
+		y.style.display = "block";
+
+    }
+}
+
+document.querySelector('.btm-hanoi').onclick = ocultarTorres;
+document.querySelector('.btm-encriptador').onclick = ocultarTorres;
+
 
 
 
